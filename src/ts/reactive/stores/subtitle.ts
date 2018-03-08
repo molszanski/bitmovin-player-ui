49087ac89aefce5f2d30c2dc2486c82175c36648
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2018-03-08 13:54:32 +0100

    - refactor and add local storage

--- src/ts/reactive/stores/subtitle.ts
+++ src/ts/reactive/stores/subtitle.ts
@@ -1,17 +1,18 @@
-import { observable, action, IObservableValue } from 'mobx';
+import { observable, action, IObservableValue, autorun } from 'mobx';
 import { StorageUtils } from '../../storageutils';
 import { RootStore } from './root';
 
-export interface SubtitleSettings {
-  fontColor: string;
-  fontOpacity: string;
-  fontFamily: string;
-  fontSize: string;
-  characterEdge: string;
-  backgroundColor: string;
-  backgroundOpacity: string;
-  windowColor: string;
-  windowOpacity: string;
+export interface SubtitleProps {
+  [name: string]: string;
+  fontColor?: string;
+  fontOpacity?: string;
+  fontFamily?: string;
+  fontSize?: string;
+  characterEdge?: string;
+  backgroundColor?: string;
+  backgroundOpacity?: string;
+  windowColor?: string;
+  windowOpacity?: string;
 }
 
 const configurableProperties = [
@@ -26,54 +27,58 @@ const configurableProperties = [
   'windowOpacity',
 ];
 
-export interface Properties extends SubtitleSettings {
-  [name: string]: string;
-}
-
-export class SubtitleStore implements SubtitleSettings {
+export class SubtitleStore implements SubtitleProps {
   [name: string]: any; // So we can iterate
   private root: RootStore;
   private localStorageKey: string;
-  private userSettings: SubtitleSettings;
 
-  @observable public fontColor: string;
-  @observable public fontOpacity: string;
-  @observable public fontFamily: string;
-  @observable public fontSize: string;
-  @observable public characterEdge: string;
-  @observable public backgroundColor: string;
-  @observable public backgroundOpacity: string;
-  @observable public windowColor: string;
-  @observable public windowOpacity: string;
+  private disposer: any; // To remove a listener
+
+  @observable public fontColor: string = 'null';
+  @observable public fontOpacity: string = 'null';
+  @observable public fontFamily: string = 'null';
+  @observable public fontSize: string = 'null';
+  @observable public characterEdge: string = 'null';
+  @observable public backgroundColor: string = 'null';
+  @observable public backgroundOpacity: string = 'null';
+  @observable public windowColor: string = 'null';
+  @observable public windowOpacity: string = 'null';
 
   constructor(rootStore: any) {
     this.root = rootStore;
     this.localStorageKey = 'temp-subtitlesettings';
 
-    // Provide some defaults
-    for (let propertyName of configurableProperties) {
-      this[propertyName] = observable.box('null');
-    }
+    this.loadSettingsFromLocalStorage();
+    // Run on every property change
+    this.disposer = autorun(() => {
+      this.saveToLocalStorage();
+    });
+
     return this;
   }
 
-  // @action
-  // public reset(): void {
-  //   for (let propertyName of SubtitleProperties) {
-  //     this[propertyName] = 'null';
-  //   }
-  // }
+  @action
+  public reset(): void {
+    for (let propertyName of configurableProperties) {
+      this[propertyName] = 'null';
+    }
+  }
 
-  // public saveToLocalStorage(): void {
-  //   StorageUtils.setObject(this.localStorageKey, this.userSettings);
-  // }
+  public saveToLocalStorage(): void {
+    let toSave: SubtitleProps = {};
+    for (let propertyName of configurableProperties) {
+      toSave[propertyName] = this[propertyName];
+    }
+    StorageUtils.setObject(this.localStorageKey, toSave);
+  }
 
-  // public loadSettingsFromLocalStorage(): void {
-  //   this.userSettings = StorageUtils.getObject<SubtitleSettings>(this.localStorageKey) || {};
+  @action
+  public loadSettingsFromLocalStorage(): void {
+    let cache: SubtitleProps = StorageUtils.getObject<SubtitleProps>(this.localStorageKey) || {};
 
-  //   // Apply the loaded settings
-  //   for (let property in this.userSettings) {
-  //     this._properties[property].value = (<any>this.userSettings)[property];
-  //   }
-  // }
+    // Apply the loaded settings
+    for (let propertyName of configurableProperties) {
+      this[propertyName] = cache[propertyName];
+    }
+  }
 }
